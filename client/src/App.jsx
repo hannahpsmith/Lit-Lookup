f3d293e115d1bf6ea9c7530a19d3670b38b71651
hannahpsmith <166345156+hannahpsmith@users.noreply.github.com>
2024-09-12 13:03:23 -0600

    updated server.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import Navbar from './components/Navbar'
 
-import Navbar from './components/Navbar';
 
 function App() {
   return (
--- server/server.js
+++ server/server.js
@@ -2,9 +2,23 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware, // Pass the authentication middleware to Apollo Server
+});
+
+server.start().then(() => {
+  app.use('/graphql', expressMiddleware(server));
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,6 +29,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
