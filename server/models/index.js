38d7cc3b8698e24881210d62f776e944b1c219a6
hannahpsmith <166345156+hannahpsmith@users.noreply.github.com>
2024-09-12 15:33:26 -0600

    defined graphQL typeDefs and resolvers

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,7 @@ const { gql } = require('apollo-server-express');
 const User = require('./User');
 const Book = require('./book');
 
-const typeDefs = gql
+const typeDefs = gql`
     type User {
         _id: ID
         username: String
@@ -36,6 +36,65 @@ const typeDefs = gql
         token: String
         user: User
   }
-;
+`;
 
-module.exports = { typeDefs, resolvers };
+const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+        if (!context.user) {
+          throw new Error('Not logged in');
+        }
+        return await User.findById(context.user._id);
+      },
+      user: async (parent, { id }) => {
+        return await User.findById(id);
+      },
+    },
+
+    Mutation: {
+      addUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user };
+      },
+
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error('No user found with this email');
+        }
+        const correctPw = await user.isCorrectPassword(password);
+        if (!correctPw) {
+          throw new Error('Incorrect password');
+        }
+        const token = signToken(user);
+        return { token, user };
+      },
+
+      saveBook: async (parent, { bookId }, context) => {
+        if (!context.user) {
+          throw new Error('Not logged in');
+        }
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      },
+      
+      removeBook: async (parent, { bookId }, context) => {
+        if (!context.user) {
+          throw new Error('Not logged in');
+        }
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      },
+    },
+  };
+
+module.exports = { User, typeDefs, resolvers };
