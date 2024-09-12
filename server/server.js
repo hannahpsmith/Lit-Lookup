404353b8e8a72b9e32dfc43165f6d94516f99167
hannahpsmith <166345156+hannahpsmith@users.noreply.github.com>
2024-09-12 14:49:39 -0600

    updated server.js and auth.js

--- server/server.js
+++ server/server.js
@@ -6,22 +6,36 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
+const { json } = require('body-parser');
+const { error } = require('console');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
+async function startApolloServer() {
+  const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware, // Pass the authentication middleware to Apollo Server
-});
+  context: ({ req }) => {
+    const { user, message } = authMiddleware({ req });
+    
+    if(message) {
+      console.log('Apollo Server Error:', error);
+    }
 
-server.start().then(() => {
+    return { user, error };
+    
+  },
+  });
+
+  await server.start();
+
   app.use('/graphql', expressMiddleware(server));
-});
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -34,6 +48,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-db.once('open', () => {
+db.once('open', async () => {
+  await startApolloServer();
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
