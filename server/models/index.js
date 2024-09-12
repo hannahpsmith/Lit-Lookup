c7c56fb0f46308b4964cf06483118ab0d03f3842
hannahpsmith <166345156+hannahpsmith@users.noreply.github.com>
2024-09-12 15:26:50 -0600

    defined graphQL resolvers

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,41 @@
+const { gql } = require('apollo-server-express');
 const User = require('./User');
+const Book = require('./book');
 
-module.exports = { User };
+const typeDefs = gql
+    type User {
+        _id: ID
+        username: String
+        email: String
+        savedBooks: [Book]
+        bookCount: Int
+    }
+    
+    type Book {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Query {
+        me: User
+        user(id: ID!): User
+  }
+
+  type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(bookId: String!): User
+        removeBook(bookId: String!): User
+  }
+
+  type Auth {
+        token: String
+        user: User
+  }
+;
+
+module.exports = { typeDefs, resolvers };
